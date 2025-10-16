531e696ae31d384a9ad9e7861e192fd57cbcce5d
tikaani <romik152006@mail.ru>
2025-10-17 00:21:38 +0300

    migrate to navbar

--- app/src/main/java/com/tikaan/libraryapp/MainActivity.java
+++ app/src/main/java/com/tikaan/libraryapp/MainActivity.java
@@ -3,11 +3,13 @@ package com.tikaan.libraryapp;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.fragment.app.Fragment;
 import androidx.appcompat.widget.Toolbar;
 
-import com.tikaan.libraryapp.adapter.BookCardsAdapter;
-import com.tikaan.libraryapp.adapter.VerticalSpaceItemDecoration;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.tikaan.libraryapp.fragments.FavouriteFragment;
+import com.tikaan.libraryapp.fragments.HomeFragment;
+import com.tikaan.libraryapp.fragments.SearchFragment;
 import com.tikaan.libraryapp.model.BookModel;
 
 import java.util.ArrayList;
@@ -23,15 +25,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setCustomActionBar();
-        generateData();
+        setupNavBar();
 
-
-
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        //generateData();
+        /*RecyclerView recyclerView = findViewById(R.id.recyclerView);
         BookCardsAdapter bookCardsAdapter = new BookCardsAdapter(this.getLayoutInflater(), books);
         recyclerView.addItemDecoration(new VerticalSpaceItemDecoration(50));
-        recyclerView.setAdapter(bookCardsAdapter);
+        recyclerView.setAdapter(bookCardsAdapter);*/
     }
+
+    private void setupNavBar(){
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        Fragment fragmentHome = new HomeFragment();
+        Fragment searchFragment = new SearchFragment();
+        Fragment favouriteFragment = new FavouriteFragment();
+
+        setCurrentFragment(fragmentHome);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_home) {
+                setCurrentFragment(fragmentHome);
+            } else if (id == R.id.nav_search) {
+                setCurrentFragment(searchFragment);
+            } else if (id == R.id.nav_favorite) {
+                setCurrentFragment(favouriteFragment);
+            }
+            return true;
+        });
+    }
+
+    private void setCurrentFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.flFragment, fragment)
+                .commit();
+    }
+
 
     private void setCustomActionBar(){
         Toolbar toolbar = findViewById(R.id.toolbar);
